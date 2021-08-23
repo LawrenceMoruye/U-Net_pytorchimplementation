e7acaf9fd71f7057d701e49d40a37666b55ba246
LawrenceMoruye <moruyelawrence@gmail.com>
2021-08-23 21:52:05 +0000

    U-net env set up

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Convolutional Networks for Biomedical',
+    author='Lawrence Moruye',
+    license='MIT',
+)
