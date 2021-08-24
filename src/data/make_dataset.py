a9812264daebf865eb94c9f703308b39ee02639f
LawrenceMoruye <moruyelawrence@gmail.com>
2021-08-24 11:40:21 +0000

    dataset class to output image and mask

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,88 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import torch
+import numpy as np
+import pandas as pd 
+import os
+import glob
+from PIL import Image,ImageFile 
+from collections import defaultdict 
+from tqdm import tqdm 
+from torchvision import transforms 
+from albumentations import (
+    Compose,
+    OneOf,
+    RandomBrightnessContrast,
+    RandomGamma,
+    ShiftScaleRotate,
+)
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+class SegmentationDataset(torch.utils.data.Dataset):
+    def __init__(self,image_ids,transform=True,preprocessing_img_fxn=None):
+        self.data = defaultdict(dict)
+        self.transform = transform
+        self.preprocessing_img_fxn =preprocessing_img_fxn
+        self.aug = Compose(
+            [
+                ShiftScaleRotate
+                (
+                shift_limit =0.0625,
+                scale_limit =0.1,
+                rotate_limit =10,
+                p=0.8
+                ),
+            OneOf(
+                [
+                    RandomGamma(
+                        gamma_limit = (90,110)
+                        ),
+                        RandomBrightnessContrast(
+                            brightness_limit =0.1,
+                            contrast_limit =0.1
+                        ),
+                   
+                ],
+                p=0.5,
+            ),
+            ]
+        )
+        for img_id in image_ids:
+            files = glob.glob(os.Path.join(TRAIN_PATH,img_id,"*.png"))
+            self.data[counter] = {
+                "img_path":os.path.join(
+                    TRAIN_PATH,img_id +".png"
+                ),
+                "mask_path":os.path.join(
+                    TRAIN_PATH,img_id + "_mask.png"
+                ),
+            }
+    def __len__(self):
+        return len(self.data)
 
+    def __getitem__(self,item):
+        img_path = self.data[item]["img_path"]
+        mask_path = self.data[item]["mask_path"]
+        
+        #read_image and convert it to RGB
+        img = Image.open(img_path)
+        img = img.convert("RGB")
+        #img to numpy array
+        img = np.array(img)
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+        #read mask
+        mask = Image.open(mask_path)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+        #convrting to binary float matrix
+        mask = (mask >=1).astype("float32")
+        #apply transform to train data only
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+        if self.transform is True:
+            augmented =self.aug(image =img,mask=mask)
+            img = augmented['image']
+            mask = augmented['mask']
 
-    main()
+        img = self.preprocessing_img_fxn(img)
+        return {
+            "image":transforms.ToTensor()(img),
+            "mask" :transforms.ToTensor()(mask).float()
+        }
